d7460de6a0e13c37032b249d5f9bba84ea72cc1e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-20 13:27:14 -0400

    innit

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
-  })
-}
+ if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+      scope: "/ICS2O-PWA-Test/",
+    })
+  }
+
+  function myButtonClicked() {
+    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  }
