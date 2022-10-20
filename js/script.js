e364c0343cce92de3ad750f1a418185712a27cd6
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-20 13:22:06 -0400

    html and js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // This file contains the JS functions for index.html
 
 "use strict"
+
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+      scope: "/ICS2O-PWA-Test/",
+    })
+  }
