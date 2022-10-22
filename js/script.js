ddb082bf78adb8ec28ec5f524c30a842190dfba6
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-22 12:05:55 -0400

    ics20-template

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-template/sw.js", {
+    scope: "/ICS2O-template/",
   })
 }
 
